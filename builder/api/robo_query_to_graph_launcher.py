6d2f935c9ffde555f69b2facad2e188d572b3a9a
Colin Curtis <ckcurtis@renci.org>
2018-08-14 22:50:43 -0400

    converted returned values from strings to json objects - dictionaries

--- builder/api/robo_query_to_graph_launcher.py
+++ builder/api/robo_query_to_graph_launcher.py
@@ -9,7 +9,6 @@ from greent.services.robo_query_to_graph import RoboQueryToGraph
 from greent.servicecontext import ServiceContext
 
 app = Flask(__name__)
-
 template = {
   "swagger": "2.0",
   "info": {
@@ -25,8 +24,8 @@ template = {
     "version": "0.0.1"
   },
   "schemes": [
-    "https",
-    "http"
+    "http",
+    "https"
   ]
 }
 app.config['SWAGGER'] = {
@@ -37,11 +36,9 @@ swagger = Swagger(app, template=template)
 #core = RoboQueryToGraph(ServiceContext.create_context ())
 #cache = LRU (1000)
 
-
-#BUTTON 1
 @app.route('/api/builder_query/<curie>/<curie_name>')
 def builder_query (curie, curie_name):
-   """ initiate a graph query with ROBOKOP Builder and return the task id
+   """ Initiate a graph query with ROBOKOP Builder and return the task id
    ---
    parameters:
      - name: curie
@@ -61,7 +58,7 @@ def builder_query (curie, curie_name):
        type: string
        required: true
        default: Ebola hemorrhagic fever
-       description: "Enter an ontological ID."
+       description: "Enter a corresponding name."
        x-valueType:
          - http://schema.org/string
        x-requestTemplate:
@@ -71,10 +68,8 @@ def builder_query (curie, curie_name):
      200:
        description: ...
    """
-
    assert curie, "A string must be entered as a query."
    assert curie_name, "A string..."
-
    builder_query_1_url = "http://127.0.0.1:6010/api/"
    builder_query_1_headers = {
      'accept' : 'application/json',
@@ -94,10 +89,8 @@ def builder_query (curie, curie_name):
               ],
               "nodes": [
                 {
-                  "MONDO":"0005737",
                   "curie": curie,
                   "id": 0,
-                  #"name": "Ebola hemorrhagic fever", aren't curie and name redundant?
                   "name": curie_name,
                   "type": "disease"
                 },
@@ -112,14 +105,11 @@ def builder_query (curie, curie_name):
               ]
             }
           }
+   builder_query_1_response = requests.post(builder_query_1_url, \
+    headers = builder_query_1_headers, json = builder_query_1_data)
+   builder_task_id_string = builder_query_1_response.json()
+   return json.dumps(builder_task_id_string)
 
-   builder_query_1_response = requests.post(builder_query_1_url, headers = builder_query_1_headers, json = builder_query_1_data)
-   task_id_string_raw = builder_query_1_response.text
-   task_id_dict = json.loads(task_id_string_raw)
-   task_id_string_clean = task_id_dict['task id']
-   return (task_id_string_raw)
-
-#BUTTON 2
 @app.route('/api/builder_status_check/<task_id>')
 def builder_status_check (task_id):
    """ Use the Task ID from Step 1 to check on the graph process.
@@ -140,97 +130,85 @@ def builder_status_check (task_id):
      200:
        description: ...
    """
-   assert task_id, "A string."
+   assert builder_task_id, "A string."
+   builder_task_status_url = "http://127.0.0.1:6010/api/task/"+builder_task_id
+   builder_task_status_response = requests.get(builder_task_status_url)
+   builder_status = builder_query_2_response.json()
+   return json.dumps(builder_status)
 
-   builder_query_2_url = "http://127.0.0.1:6010/api/task/"+task_id
-   builder_query_2_headers = {
-     'accept' : 'application/json'
-   }
-   builder_query_2_response = requests.get(builder_query_2_url)
-   check_builder_status_raw = builder_query_2_response.text
-   check_builder_status_dict = json.loads(check_builder_status_raw)
-   builder_status = check_builder_status_dict['status']
-   print(builder_status)
-   
-   return (builder_status)
-
-#BUTTON 3
-@app.route('/api/querytograph/step_3_ranker')
-def query_to_robo_ranker (XYZ):
-   """ Use the Task ID from Step 1 to check on the graph process.
+@app.route('/api/ranker_query/<curie>/<curie_name>')
+def query_to_robo_ranker (curie, curie_name):
+   """ Initiate a graph ranking process with ROBOKOP Ranker and return the task id
    ---
    parameters:
-     - name: machine input
+     - name: curie
        in: path
        type: string
        required: true
-       default: ef42004e-13f7-45a8-bf9b-75ffa2c50b40
-       description: "Enter a task ID from Builder Step 1."
+       default: MONDO:0005735
+       description: "Enter an Ontological ID."
        x-valueType:
          - http://schema.org/string
        x-requestTemplate:
          - valueType: http://schema.org/string
-           template: /is_a/{{ input }}/{{ input2 }}
-     
-          
+           template: /ranker_query_curie/{{ input }}/{{ input2 }}
+     - name: curie_name
+       in: path
+       type: string
+       required: true
+       default: Ebola hemorrhagic fever
+       description: "Enter a corresponding name."
+       x-valueType:
+         - http://schema.org/string
+       x-requestTemplate:
+         - valueType: http://schema.org/string
+           template: /ranker_query_curie_name/{{ input }}/{{ input2 }}  
    responses:
      200:
        description: ...
    """
-   assert task_id, "A string must be entered as a query."
-
-   builder_query_2_url = "http://127.0.0.1:6010/api/task/"+task_id
-   builder_query_2_headers = {
-     'accept' : 'application/json'
+   assert curie, "A string must be entered as a query."
+   assert curie_name, "A string..."
+   ranker_query_1_url = "http://127.0.0.1:6011/api/"
+   ranker_query_1_headers = {
+     'accept' : 'application/json',
+     'Content-Type' : 'application/json' 
    }
-   builder_query_2_response = requests.get(builder_query_2_url)
-   check_builder_status_raw = builder_query_2_response.text
-   check_builder_status_dict = json.loads(check_builder_status_raw)
-   builder_status = check_builder_status_dict['status']
-   print(builder_status)
-   
-   return (builder_status)
-
-#BUTTON 4
-# @app.route('/api/querytograph/all_steps_combined')
-# def query_to_graph_all_steps_combined (search_term = "MONDO:0005737"):
-#    """ Not Currently Functional
-#    ---
-#    parameters:
-#      - name: search term of interest
-#        in: path
-#        type: string
-#        required: false
-#        default: MONDO:0005737
-#        description: "Enter an ontological ID."
-#        x-valueType:
-#          - http://schema.org/string
-#        x-requestTemplate:
-#          - valueType: http://schema.org/string
-#            template: /is_a/{{ input }}/{{ input2 }}
-     
-          
-#    responses:
-#      200:
-#        description: ...
-#    """  
-#    return
-
-#assert curie, "A string must be entered as a query."
-   #assert name, "A string must be entered as a query."
-  
-   # the below separation of keys ensures that a search for q = 'diabetes' returns
-   # a result distinct and different if concept = 'drug' OR concept = 'disease'
-   # any unique, two-term search will yield distinct results and cache them
-  #  curie_key = f"{curie}"
-  #  name_key = f"{name}"
-  #  full_key = curie_key + name_key 
-  #  if full_key in cache:
-  #     result = cache[full_key]
-  #  else:  
-  #     result = core.lookup_router(curie_key, name=name_key)
-  #     cache[full_key] = result
-  #  return jsonify(result)
+   ranker_query_1_data = {
+            "machine_question": {
+              "edges": [
+                {
+                  "source_id": 0,
+                  "target_id": 1,
+                  "type": "disease_to_gene_association"
+                },
+                {
+                  "source_id": 1,
+                  "target_id": 2,
+                  "type": "has_phenotype"
+                }
+              ],
+              "nodes": [
+                {
+                  "curie": curie,
+                  "id": 0,
+                  "type": "disease"
+                },
+                {
+                  "id": 1,
+                  "type": "gene"
+                },
+                {
+                  "id": 2,
+                  "type": "genetic_condition"
+                }
+              ]
+            }
+          }
+   ranker_query_1_response = requests.post(ranker_query_1_url, \
+    headers = ranker_query_1_headers, json = ranker_query_1_data)
+   ranker_task_id_string = ranker_query_1_response.json()
+   return json.dumps(ranker_task_id_string)
 
 if __name__ == "__main__":
    parser = argparse.ArgumentParser(description='Robo_query_to_graph Server')
