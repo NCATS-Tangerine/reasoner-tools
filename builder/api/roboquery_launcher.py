22aa69c8ee95bfc0e6359287e1232732384b91f0
Colin Curtis <ckcurtis@renci.org>
2018-08-17 09:37:37 -0400

    initial commit for adding second fcn to process N MQs automatically

--- builder/api/roboquery_launcher.py
+++ builder/api/roboquery_launcher.py
@@ -6,6 +6,7 @@ import os
 import requests
 import logging
 import time
+import jsonpath_rw
 from datetime import timedelta
 from flask import Flask, jsonify, g, Response, request
 from flasgger import Swagger
@@ -18,7 +19,7 @@ from builder.api.roboquery_setup import app, api
 
 logger = logging.getLogger("roboquery")
 
-class BuildAndRank(Resource):
+class BuildAndRankOneQuestion(Resource):
     def __init__(self, ranker_answer = {}):
         self.ranker_answer = ranker_answer
         return
@@ -37,13 +38,13 @@ class BuildAndRank(Resource):
                 $ref: '#/definitions/Question'
             required: true
         responses:
+            200:
+                description: successful operation
             202:
-                description: Building onto KG and Ranking results...
-                schema:
-                    type: object
-                    properties:
-                        machine question:
-                            description: machine-readable question graph                
+                description: Building onto KG and Ranking results
+            400:
+                description: Invalid input value(s)
+                     
         """
         # replace `parameters`` with this when OAS 3.0 is fully supported by Swagger UI
         # https://github.com/swagger-api/swagger-ui/issues/3641
@@ -59,6 +60,7 @@ class BuildAndRank(Resource):
         # First we queue an update to the Knowledge Graph (KG) using ROBOKOP builder
         logger = logging.getLogger('builder KG update task')
         logger.info("Queueing 'KG update' task...")
+        
         builder_query_1_url = "http://127.0.0.1:6010/api/"
         builder_query_1_headers = {
           'accept' : 'application/json',
@@ -73,6 +75,7 @@ class BuildAndRank(Resource):
         #now query ROBOKOP Builder for the status of Knowledge Graph work
         logger = logging.getLogger('builder KG update status query')
         logger.info("Checking status of 'KG update' task...")
+        
         break_loop = False
         while not break_loop:
           time.sleep(2)
@@ -98,41 +101,119 @@ class BuildAndRank(Resource):
         #print(self.ranker_answer)
         return self.ranker_answer        
 
-api.add_resource(BuildAndRank, '/buildrank')
+api.add_resource(BuildAndRankOneQuestion, '/OneQuestion')
 
 
-
-class Refine(Resource):
-    def __init__(self):
-        self.test = BuildAndRank()
-        print(self.test.post())
+class BuildAndRankNQuestions(Resource):
+    def __init__(self, ranker_answer = {}):
+        self.ranker_answer = ranker_answer
+        return
         
-        
-
-
-
-    def post(self):
+    def post(self, ranker_answer={}):
         """ 
-        Refining our previous KG response and performing a new reasoning operation...
+        Initiate a graph query with ROBOKOP Builder and return a Graph with rankings from ROBOKOP Ranker.
         ---
         tags: [RoboQuery]
         parameters:
           - in: body
-            name: refine
-            description: The output of the previous Build & Rank operation is utilized, with or without
-                revision, to get back a more-refined Knowledge Graph.
+            name: build & rank
+            description: A machine-readable question graph, entered here, will build
+                onto the Knowledge Graph (KG) and return a portion of that KG with rank values.
             schema:
                 $ref: '#/definitions/Question'
             required: true
-        
+        responses:
+            200:
+                description: successful operation
+            202:
+                description: Building onto KG and Ranking results
+            400:
+                description: Invalid input value(s)
+                     
         """
-        # test = BuildAndRank()
-        # print(test.ranker_answer)
-        return
+        # replace `parameters`` with this when OAS 3.0 is fully supported by Swagger UI
+        # https://github.com/swagger-api/swagger-ui/issues/3641
+        """
+        requestBody:
+            description: The machine-readable question graph.
+            required: true
+            content:
+                application/json:
+                    schema:
+                        $ref: '#/definitions/Question'
+        """
+        # First we queue an update to the Knowledge Graph (KG) using ROBOKOP builder
+        logger = logging.getLogger('builder KG update task')
+        logger.info("Queueing 'KG update' task...")
+        
+        builder_query_1_url = "http://127.0.0.1:6010/api/"
+        builder_query_1_headers = {
+          'accept' : 'application/json',
+          'Content-Type' : 'application/json'
+          }
+        builder_query_1_data = request.json
+        builder_query_1_response = requests.post(builder_query_1_url, \
+          headers = builder_query_1_headers, json = builder_query_1_data)
+        builder_task_id = builder_query_1_response.json()
+        builder_task_id_string = builder_task_id["task id"]
 
+        #now query ROBOKOP Builder for the status of Knowledge Graph work
+        logger = logging.getLogger('builder KG update status query')
+        logger.info("Checking status of 'KG update' task...")
+        
+        break_loop = False
+        while not break_loop:
+          time.sleep(2)
+          builder_task_status_url = "http://127.0.0.1:6010/api/task/"+builder_task_id_string
+          builder_task_status_response = requests.get(builder_task_status_url)
+          builder_status = builder_task_status_response.json()
+          if builder_status['status'] == 'SUCCESS':
+            break_loop = True
 
-api.add_resource(Refine, '/refine')
+        #KG has been updated by Builder, get answers NOW from ROBOKOP Ranker!
+        logger = logging.getLogger('Ranker Answer query')
+        logger.info("Getting Answers about KG from Ranker...")
 
+        ranker_now_query_url = "http://127.0.0.1:6011/api/now"
+        ranker_now_query_headers = {
+          'accept' : 'application/json',
+          'Content-Type' : 'application/json'
+          }
+        ranker_now_query_data = request.json
+        ranker_now_query_response = requests.post(ranker_now_query_url, \
+          headers = builder_query_1_headers, json = builder_query_1_data)
+        self.ranker_answer = ranker_now_query_response.json()
+        #print(self.ranker_answer)
+        return self.ranker_answer        
+
+api.add_resource(BuildAndRankNQuestions, '/NQuestions')
+
+# class Refine(Resource):
+#     def __init__(self):
+#         self.test = BuildAndRank()
+    
+#     def post(self):
+#         """ 
+#         Refining our previous KG response and performing a new reasoning operation...
+#         ---
+#         tags: [RoboQuery]
+#         parameters:
+#           - in: body
+#             name: refine
+#             description: The output of the previous Build & Rank operation is utilized, with or without
+#                 revision, to get back a more-refined Knowledge Graph.
+#             schema:
+#                 $ref: '#/definitions/Question'
+#             required: true
+#         responses:
+#             200:
+#                 description: successful operation
+#             400:
+#                 description: Invalid input value(s)
+#         """
+#         return
+
+# api.add_resource(Refine, '/refine')
 
 if __name__ == '__main__':
 
