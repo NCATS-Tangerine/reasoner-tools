2a3af2c5305d2e68d61d13b6b0cf4d9a2187ba89
Steven Cox <scox@renci.org>
2018-08-27 21:53:33 -0400

    workflow

--- deploy/rosetta/task_queue_wrapper.sh
+++ deploy/rosetta/task_queue_wrapper.sh
@@ -6,7 +6,7 @@ ROOT=/code/reasoner-tools
 git pull
 pip install -r greent/requirements.txt
 while true; do
-#    su - rosetta -c "$ROOT/task_queue.sh"
+    #    su - rosetta -c "$ROOT/task_queue.sh"
     $ROOT/task_queue.sh
     sleep 10
 done
