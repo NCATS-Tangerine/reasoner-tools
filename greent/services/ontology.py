bfbc3b92473b28e1f7a67b853495f7ade58c58af
Colin Curtis <ckcurtis@renci.org>
2018-10-11 19:59:57 -0400

    diagnostic prints on 218,219

--- greent/services/ontology.py
+++ greent/services/ontology.py
@@ -215,7 +215,7 @@ class GenericOntology(Service):
             property_value = [x for x in ontology_entry.other['property_value']]
         else:
             property_value = [x for x in ontology_entry.other['property_value'] if x.startswith(property_key)]
+            print('property_value:', property_value)
+            print('property_value[0]:', property_value[0])
             property_value = property_value[0].replace(property_key+' "', "").replace('" xsd:string', "")
-        
-        
         return property_value
