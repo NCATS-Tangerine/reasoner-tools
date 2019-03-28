93615f18396a6bb8cfd7a9210d38710d0ff1384e
Colin Curtis <ckcurtis@renci.org>
2019-03-28 14:24:30 -0400

    fixed typos in usage cmd

--- greent/api/uberonto_wsgi.py
+++ greent/api/uberonto_wsgi.py
@@ -1,4 +1,4 @@
-# usage: gunicorn --workers=# --bind:0.0.0.0:5000 uberonto_wsgi:app
+# usage: gunicorn --workers=3 --bind=0.0.0.0:5000 uberonto_wsgi:app
 from uberonto import app
 
 if __name__ == "__main__":
