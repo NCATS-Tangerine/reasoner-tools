a5760aaed1c07f8276280086884b2cb30c709839
Colin Curtis <ckcurtis@renci.org>
2019-03-29 12:24:07 -0400

    updated usage cmd

--- greent/api/onto_wsgi.py
+++ greent/api/onto_wsgi.py
@@ -1,4 +1,4 @@
-# usage: gunicorn --workers=3 --bind=0.0.0.0:5000 --pythonpath '../../' onto_wsgi:app
+# usage: gunicorn --workers=3 --timeout=120 --bind=0.0.0.0:5000 --pythonpath '../../' onto_wsgi:app
 from onto_gunicorn import app
 
 if __name__ == "__main__":
