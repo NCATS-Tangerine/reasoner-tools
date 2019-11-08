d08d586f3bd2d76b48784b56dd2edd19dd9779d4
Chuck Bennett <cnbennett3@gmail.com>
2019-11-08 17:04:31 -0500

    Commit onto NFS logging for kubernetes.

--- docker/onto_gunicorn/onto_gunicorn_smartapi.sh
+++ docker/onto_gunicorn/onto_gunicorn_smartapi.sh
@@ -13,14 +13,24 @@ if [ "x$GIT_PULL" == "xtrue" ]; then
     cd greent/api
 fi
 
-cd /code/reasoner-tools/greent/api
+if [ -z $ACCESS_LOG ]; then
+    export ACCESS_LOG='-'
+fi
 
+if [ -z $ERROR_LOG ]; then
+    export ERROR_LOG='-'
+fi
+
+cd /code/reasoner-tools/greent/api
 gunicorn \
         --bind=0.0.0.0:$APP_PORT \
         --workers=$NUM_WORKERS \
         --pythonpath '../../' \
         --timeout=120 \
+        --access-logfile $ACCESS_LOG \
+        --error-logfile $ERROR_LOG \
+        --log-level debug \
         onto_gunicorn:app
 
 echo Exiting.
-exit
+exit 0
